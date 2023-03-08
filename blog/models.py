6a933e0663bc75e9722b3254287ce212a5274109
Molly Adamsson <adamssonmolly@gmail.com>
2023-03-08 12:35:33 +0000

    Add corrections

--- blog/models.py
+++ blog/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 from cloudinary.models import CloudinaryField
 
+
 STATUS = ((0, "Draft"), (1, "Published"))
 
 
@@ -20,14 +21,27 @@ class Post(models.Model):
     likes = models.ManyToManyField(
         User, related_name='blogpost_like', blank=True)
 
+    class Meta:
+        ordering = ["-created_on"]
 
-class Meta:
-    ordering = ["-created_on"]
+    def __str__(self):
+        return self.title
+
+    def number_of_likes(self):
+        return self.likes.count()
 
 
-def __str__(self):
-    return self.title
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE,
+                             related_name="comments")
+    name = models.CharField(max_length=80)
+    email = models.EmailField()
+    body = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    approved = models.BooleanField(default=False)
 
+    class Meta:
+        ordering = ["created_on"]
 
-def number_of_likes(self):
-    return self.likes.count()
+    def __str__(self):
+        return f"Comment {self.body} by {self.name}"
